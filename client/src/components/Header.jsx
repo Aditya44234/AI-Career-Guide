3b9bea0c13979761c865f4961d766347f346b130
Aditya joshi <168006690+Aditya44234@users.noreply.github.com>
2025-10-18 23:47:56 +0530

    Logo added

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,8 @@ export default function Header() {
   return (
     <header className="bg-white shadow fixed top-0 w-full z-40">
       <nav className="container mx-auto flex justify-between items-center p-4">
-        <Link to="/" className="text-xl font-bold text-indigo-600">
+        <Link to="/" className="text-xl font-bold text-indigo-600 flex items-center justify-center">
+        {/* <img src="/logo.jpg" width={60}  alt="" /> */}
           AI Career Guide
         </Link>
         <div className="space-x-4">
